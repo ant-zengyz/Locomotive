1aa6c957883186b0f096600161d010fa651d66c3
zengyuanzheng <zengyuanzheng@fenghuangjinfu.com>
2018-10-17 16:49:56 +0800

    添加管理员模块

---
+++ locomotive-administrator/src/main/java/com/example/locomotive/administrator/LocomotiveAdministratorApplication.java
@@ -0,0 +1,12 @@
+package com.example.locomotive.administrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocomotiveAdministratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocomotiveAdministratorApplication.class, args);
+    }
+}
