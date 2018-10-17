4d3bbdd0a3cb42c53c0d461079b90e41ac48ca5e
zengyuanzheng <zengyuanzheng@fenghuangjinfu.com>
2018-10-17 16:44:22 +0800

    添加占位包

---
+++ locomotive-common/src/main/java/com/example/locomotive/common/constant/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 描述：常量包
+ * User: 曾远征
+ * Date: 2018-10-17
+ * Time: 16:42
+ */
+package com.example.locomotive.common.constant;
---
+++ locomotive-common/src/main/java/com/example/locomotive/common/utils/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 描述：工具包
+ * User: 曾远征
+ * Date: 2018-10-17
+ * Time: 16:43
+ */
+package com.example.locomotive.common.utils;
