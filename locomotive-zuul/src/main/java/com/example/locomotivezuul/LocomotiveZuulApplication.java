093dc423e7fc89659096717dbf18d67a0bca669f
zengyuanzheng <zengyuanzheng@fenghuangjinfu.com>
2018-10-20 13:16:30 +0800

    添加zuul路由模块

---
+++ locomotive-zuul/src/main/java/com/example/locomotivezuul/LocomotiveZuulApplication.java
@@ -0,0 +1,12 @@
+package com.example.locomotivezuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocomotiveZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocomotiveZuulApplication.class, args);
+	}
+}
---
+++ locomotive-zuul/src/test/java/com/example/locomotivezuul/LocomotiveZuulApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.locomotivezuul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LocomotiveZuulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
